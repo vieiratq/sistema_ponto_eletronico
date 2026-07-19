20303e56155a43f0dce3b08705937b9ca6a2b7ed
Joao pedro vieira <joaovieirapedro1@gmail.com>
2026-07-18 22:13:46 -0300

    feat: Adicionado conexão com db

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,4 +1,10 @@
 import express from "express";
 import path from "node:path";
+const router = express.Router();
 import { fileURLToPath } from "node:url";
+const db = require("./dataBaseRoutes.ts");
 const app = express();
+
+router.get("/", (req, res) => {
+    
+}) 
