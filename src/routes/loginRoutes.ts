18d913d56a545632fdf40685a1ac776aa106350a
Joao pedro vieira <joaovieirapedro1@gmail.com>
2026-07-20 21:39:58 -0300

    docs: docs

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require("./dataBaseRoutes.ts");
 const validaLogin = require("../functions/validaLogin.js");
 const path = require("path");
+const saltRounds = process.env.SALTROUNDS;
+const bcrypt = require("bcrypt");
 import type { Request, Response } from "express";
 
 router.get("/login", (req: Request, res: Response) => {
@@ -11,12 +13,20 @@ router.get("/login", (req: Request, res: Response) => {
 
 router.post("/login", async (req: Request, res: Response) => {
   const { cnpj, password } = req.body;
-  db.query("SELECT email FROM users WHERE cnpj = $1 ", [cnpj, password], (err: Error, result:unknown) => {
-  if(err){
-    console.log(err); 
-    return res.json({message: "Email ou senha incorreta."});  
-  }
+  const passHash = bcrypt.hashSync(password, saltRounds);
+  db.query("SELECT email,password,nome,usuarios FROM empresas WHERE cnpj = $1 ", [cnpj], (err: Error, result: any) => {
+    if (err) {
+      console.log(err);
+      return res.json({ message: "erro ao conectar ao banco de dados" });
+    }
+    const user = result.rows[0];
+    if (user.password == passHash) {
+      req.session.user = user;
+      req.session.empresa = user.nome;
+
+    }
   })
 });
 
 module.exports = router;
+export { };
